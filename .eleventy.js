e28afe7e35b2236f968083196b3e2a1681e5bac0
carson <carson@leaddyno.com>
2021-04-13 11:18:12 -0600

    start of port to tailwinds

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
-const pluginSass = require("eleventy-plugin-sass");
-
 module.exports = function(config) {
     config.addPassthroughCopy("js");
-    config.addPassthroughCopy("css");
     config.addPassthroughCopy("img");
-    config.addPlugin(pluginSass, {});
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['_site/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
